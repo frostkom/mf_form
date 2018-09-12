64a393431ececbf53a2aeca958e738209d006cb8
Martin Fors <martin.fors@frostkom.se>
2018-09-12 18:26:16 +0200

    Change the width of inline/overlay forms

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Form
 Plugin URI: https://github.com/frostkom/mf_form
 Description: 
-Version: 12.1.15
+Version: 12.1.16
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://frostkom.se
