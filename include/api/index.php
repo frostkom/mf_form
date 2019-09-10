d90e141506592d327939a2af2dce9f7f6062114b
frostkom <martin.fors@frostkom.se>
2019-09-10 10:06:11 +0200

    Use new cache functionality

--- include/api/index.php
+++ include/api/index.php
@@ -193,12 +193,13 @@ if(get_current_user_id() > 0)
 
 if($type_action == 'zipcode')
 {
-	if(is_plugin_active('mf_cache/index.php'))
+	/*if(is_plugin_active('mf_cache/index.php'))
 	{
 		$obj_cache = new mf_cache();
 		$obj_cache->fetch_request();
 		$obj_cache->get_or_set_file_content(array('suffix' => 'json'));
-	}
+	}*/
+	do_action('run_cache', array('suffix' => 'json'));
 
 	$search = str_replace(" ", "", $type_id);
 	$city_name = "";
