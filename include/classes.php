637fd7183fdf8d149f0826b4dc443440df7fa68c
frostkom <martin.fors@frostkom.se>
2025-02-05 10:22:40 +0100

    Make script load in footer

--- include/classes.php
+++ include/classes.php
@@ -333,7 +333,7 @@ class mf_form
 		$plugin_include_url = plugin_dir_url(__FILE__);
 		$plugin_version = get_plugin_version(__FILE__);
 
-		wp_register_script('script_form_block_wp', $plugin_include_url."block/script_wp.js", array('wp-blocks', 'wp-element', 'wp-components', 'wp-editor', 'wp-block-editor'), $plugin_version);
+		wp_register_script('script_form_block_wp', $plugin_include_url."block/script_wp.js", array('wp-blocks', 'wp-element', 'wp-components', 'wp-editor', 'wp-block-editor'), $plugin_version, true);
 
 		wp_localize_script('script_form_block_wp', 'script_form_block_wp', array(
 			'block_title' => __("Form", 'lang_form'),
